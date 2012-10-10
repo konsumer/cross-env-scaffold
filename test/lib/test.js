d017f347cc67e0a3512540febb876bf775d82bfd
David Galyardt <dgalyardt@janrain.com>
2012-10-10 13:16:31 -0700

    moved stuff in test dir to tidy up

--- test/lib/test.js
+++ test/lib/test.js
@@ -5,7 +5,7 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 
-command = 'phantomjs ' + __dirname + '/lib/mocha-phantomjs/lib/mocha-phantomjs.coffee ' + __dirname + '/test.html?' + process.env.TEST + ' spec';
+command = 'phantomjs ' + __dirname + '/mocha-phantomjs/lib/mocha-phantomjs.coffee ' + __dirname + '/test.html?' + process.env.TEST + ' spec';
 
 var child = exec(command,  function (error, stdout, stderr) {
 	sys.puts(stdout);
