a811c37722f97f809335eebec6a79345dc094ab2
David Konsumer <konsumer@jetboystudio.com>
2012-10-10 13:11:33 -0700

    Don't need to log command.

--- test/test.js
+++ test/test.js
@@ -6,7 +6,6 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 
 command = 'phantomjs ' + __dirname + '/lib/mocha-phantomjs/lib/mocha-phantomjs.coffee ' + __dirname + '/test.html?' + process.env.TEST + ' spec';
-console.log(command);
 
 var child = exec(command,  function (error, stdout, stderr) {
 	sys.puts(stdout);
